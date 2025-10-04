cf58a658ac9876a82cfa60723c8c1110495b978f
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-10-04 19:25:18 +0530

    hello cpp code

---
+++ Basic/hello.cpp
@@ -0,0 +1,14 @@
+//write a code hello world in c++
+
+#include <iostream>
+
+int main() 
+{
+    // std::cout is used for standard output (printing to the console)
+    std::cout << "Hello, World!" << std::endl; 
+    return 0;
+}
+
+/*output=
+Hello,World! 
+*/
